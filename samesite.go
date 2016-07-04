572cbde17141a7a5062f62138fda5abb8c7fbdec
Mike West <mike@mikewest.org>
2016-07-04 21:51:53 +0200

    Fleshing out an initial test.

--- samesite.go
+++ samesite.go
@@ -15,6 +15,18 @@ func init() {
   http.HandleFunc("/cookie/set", setCookie)
 }
 
+// Set wide-open CORS and no-cache headers on |w|, given |r|'s `Origin` header.
+func setResponseHeaders(w http.ResponseWriter, r *http.Request) {
+  origin := r.Header.Get("Origin")
+  if origin == "" {
+    origin = "*"
+  }
+  w.Header().Set("Access-Control-Allow-Origin", origin)
+  w.Header().Set("Access-Control-Allow-Credentials", "true")
+  w.Header().Set("Cache-Control", "no-cache");
+  w.Header().Set("Expires", "Fri, 01 Jan 1990 00:00:00 GMT")
+}
+
 // Respond to `/cookie/drop?name={name}` by expiring the cookie named `{name}`.
 func dropCookie(w http.ResponseWriter, r *http.Request) {
   name := r.FormValue("name")
@@ -23,19 +35,32 @@ func dropCookie(w http.ResponseWriter, r *http.Request) {
     return
   }
 
+  // Expire the named cookie, and return a JSON-encoded success code.
+  setResponseHeaders(w, r)
+  w.Header().Set("Content-Type", "application/json; charset=utf-8")
   http.SetCookie(w, &http.Cookie{Name: name, Value: "_", MaxAge: -1})
+  fmt.Fprint(w, "{\"success\": true}")
 }
 
 // Respond to `/cookie/list` by dumping the cookies contained in the request as
-// a JSON-encoded string.
+// a JSON-encoded string of Name/Value tuples.
 func listCookie(w http.ResponseWriter, r *http.Request) {
-  err := json.NewEncoder(w).Encode(r.Cookies())
+  // Strip the request cookie down to a name/value pair (as we don't actually
+  // get any additional data in the `cookie` request header).
+  requestCookies := make(map[string]string, len(r.Cookies()))
+  for _, cookie := range r.Cookies() {
+    requestCookies[cookie.Name] = cookie.Value
+  }
+
+  // Stringify the resulting array, and deliver it as JSON with wide-open CORS
+  // headers in order to allow access from everywhere.
+  setResponseHeaders(w, r)
+  w.Header().Set("Content-Type", "application/json; charset=utf-8")
+  err := json.NewEncoder(w).Encode(requestCookies)
   if err != nil {
     http.Error(w, "Failed to JSON encode the request's cookies.", http.StatusInternalServerError)
     return
   }
-
-  w.Header().Set("Content-Type", "application/json; charset=utf-8")
 }
 
 // Respond to `/cookie/set?{cookie}` by echoing `{cookie}` as a `Set-Cookie`
@@ -47,6 +72,7 @@ func setCookie(w http.ResponseWriter, r *http.Request) {
     return
   }
 
+  setResponseHeaders(w, r)
   w.Header().Add("Set-Cookie", query)
   fmt.Fprint(w, fmt.Sprintf("Wrote `%s`.", query))
 }
