21a99713103b028f48b36f0f214981a62df836e1
Mike West <mike@mikewest.org>
2016-07-05 12:29:08 +0200

    IFrame tests.

--- samesite.go
+++ samesite.go
@@ -2,6 +2,7 @@ package samesite
 
 import (
   "fmt"
+  "html/template"
   "net/http"
   "encoding/json"
 )
@@ -13,6 +14,7 @@ func init() {
   http.HandleFunc("/cookie/drop", dropCookie)
   http.HandleFunc("/cookie/list", listCookie)
   http.HandleFunc("/cookie/set", setCookie)
+  http.HandleFunc("/cookie/postToParent", postToParent)
 }
 
 // Set wide-open CORS and no-cache headers on |w|, given |r|'s `Origin` header.
@@ -42,23 +44,52 @@ func dropCookie(w http.ResponseWriter, r *http.Request) {
   fmt.Fprint(w, "{\"success\": true}")
 }
 
-// Respond to `/cookie/list` by dumping the cookies contained in the request as
-// a JSON-encoded string of Name/Value tuples.
-func listCookie(w http.ResponseWriter, r *http.Request) {
-  // Strip the request cookie down to a name/value pair (as we don't actually
-  // get any additional data in the `cookie` request header).
+// Strip |r|'s cookies down to a name/value pair (as we don't actually
+// get any additional data in the `cookie` request header).
+func extractRequestCookies(r *http.Request) map[string]string {
   requestCookies := make(map[string]string, len(r.Cookies()))
   for _, cookie := range r.Cookies() {
     requestCookies[cookie.Name] = cookie.Value
   }
+  return requestCookies
+}
 
+// Respond to `/cookie/list` by dumping the cookies contained in the request as
+// a JSON-encoded string of Name/Value tuples.
+func listCookie(w http.ResponseWriter, r *http.Request) {
   // Stringify the resulting array, and deliver it as JSON with wide-open CORS
   // headers in order to allow access from everywhere.
   setResponseHeaders(w, r)
   w.Header().Set("Content-Type", "application/json; charset=utf-8")
-  err := json.NewEncoder(w).Encode(requestCookies)
+  err := json.NewEncoder(w).Encode(extractRequestCookies(r))
   if err != nil {
     http.Error(w, "Failed to JSON encode the request's cookies.", http.StatusInternalServerError)
+    return
+  }
+}
+
+// Respond to `/cookie/postToParent` by sending the same list of cookies generated
+// for `/cookie/list` to a parent window via `postMessage`.
+func postToParent(w http.ResponseWriter, r *http.Request) {
+  setResponseHeaders(w, r)
+  w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+  const tmpl = `
+<!DOCTYPE html>
+<script>
+  var data = {{.}};
+
+  if (window.parent != window)
+    window.parent.postMessage(data, "*");
+
+  if (window.opener)
+    window.opener.postMessage(data, "*");
+</script>
+  `
+  t, err := template.New("page").Parse(tmpl)
+  err = t.Execute(w, extractRequestCookies(r))
+  if err != nil {
+    http.Error(w, "Failed to dump cookies into the JSON template.", http.StatusInternalServerError)
     return
   }
 }
--- static/tests/resources/cookie-helper.js
+++ static/tests/resources/cookie-helper.js
@@ -15,14 +15,18 @@ function assert_dom_cookie(name, value, present) {
 function assert_http_cookie(origin, name, value, present) {
   return credFetch(origin + "/cookie/list")
       .then(r => r.json())
-      .then(j => {
-        assert_equals(j[name], present ? value : undefined, "`" + name + "=" + value + "` in request to `" + origin + "`.");
+      .then(obj => {
+        assert_cookie(origin, obj, name, value, present);
       });
 }
 
+function assert_cookie(origin, obj, name, value, present) {
+  assert_equals(obj[name], present ? value : undefined, "`" + name + "=" + value + "` in request to `" + origin + "`.");
+}
+
 // Remove the cookie named |name| from |origin|, then set it on |origin| anew.
-// If |origin| matches `document.origin`, also assert that the cookie was
-// correctly removed and reset.
+// If |origin| matches `document.origin`, also assert (via `document.cookie`) that
+// the cookie was correctly removed and reset.
 function create_cookie(origin, name, value, extras) {
   return credFetch(origin + "/cookie/drop?name=" + name)
     .then(_ => {
@@ -37,3 +41,37 @@ function create_cookie(origin, name, value, extras) {
         });
     });
 }
+
+// Embed `/cookie/postToParent` on |origin|, returning a Promise which will
+// resolve with the cookie data it posts.
+function embed_cookie_posting_iframe(origin) {
+  return new Promise((resolve, reject) => {
+    var iframe = document.createElement("iframe");
+    iframe.src = origin + "/cookie/postToParent";
+    document.body.appendChild(iframe);
+    window.addEventListener("message", e => {
+      if (e.source == iframe.contentWindow) {
+        document.body.removeChild(iframe);
+        resolve(e.data);
+      }
+    });
+    iframe.onerror = reject;
+  });
+}
+
+// Set |ORIGIN|, |WWW_ORIGIN|, |SUBDOMAIN_HOST|, |CROSS_SITE_HOST| depending on the current
+// document's origin.
+(_ => {
+  var HOST = "rfc6265.biz";
+  var PORT = "";
+  var CROSS_ORIGIN_HOST = "samesite-test.appspot.com";
+  if (window.location.hostname.match(/rfc6265.test$/)) {
+    PORT = ":8080"
+    HOST = "rfc6265.test";
+    CROSS_ORIGIN_HOST = "127.0.0.1";
+  }
+  window.ORIGIN = "http://" + HOST + PORT;
+  window.WWW_ORIGIN = "http://www." + HOST + PORT;
+  window.SUBDOMAIN_ORIGIN = "http://subdomain." + HOST + PORT;
+  window.CROSS_SITE_ORIGIN = "http://" + CROSS_ORIGIN_HOST + PORT;
+})();
